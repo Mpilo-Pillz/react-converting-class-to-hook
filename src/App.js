f5641f0fe90e95d4aa21b67a458b0fa5b4ae4af0
mpilopillz <mpilo.dlamini@yahoo.com>
2021-06-25 05:04:13 +0200

    adding separate state vars for easier state updating

--- src/App.js
+++ src/App.js
@@ -4,50 +4,57 @@ import CharPicker from "./components/CharPicker";
 import Character from "./components/Character";
 
 const App = (props) => {
-  const [state, setState] = useState({
-    selectedCharacter: 1,
-    side: "light",
-    destroyed: false,
-  });
+  // *************************************************************************************************
+  // The below is a cumber some way to update state they requires the use of the spread to update like so ...state, side:side
+  // *********************************************************************************************************
+  // const [state, setState] = useState({
+  //   selectedCharacter: 1,
+  //   side: "light",
+  //   destroyed: false,
+  // });
 
-  // *****************************************************************************************************
+  // *************************************************************************************************
   // The below is a solution to stopping having to keep updating the state like this ...state, side:side
-  // *****************************************************************************************************
+  // *********************************************************************************************************
 
-  // const [destroyed, setDestroyed] = useState(false);
-  // const [selectedCharacter, setSelectedCharacter] = useState(1)
-  // const [chosenSide, setChosenSide] = useState('light');
+  const [destroyed, setDestroyed] = useState(false);
+  const [selectedCharacter, setSelectedCharacter] = useState("2");
+  const [chosenSide, setChosenSide] = useState("light");
 
   const sideHandler = (side) => {
-    setState({ ...state, side: side });
+    setChosenSide(side);
+    // setState({ ...state, side: side });
   };
 
   const charSelectHandler = (event) => {
     const charId = event.target.value;
-    setState({ ...state, selectedCharacter: charId });
+    setSelectedCharacter(charId);
+    // setState({ ...state, selectedCharacter: charId });
   };
 
   const destructionHandler = () => {
-    setState({ destroyed: true });
+    setDestroyed(true);
+    // setState({ ...state, destroyed: true });
   };
 
   let content = (
     <React.Fragment>
       <CharPicker
-        side={state.side}
-        selectedChar={state.selectedCharacter}
+        // side={state.side}
+        side={chosenSide}
+        selectedChar={selectedCharacter}
         onCharSelect={charSelectHandler}
       />
-      <Character selectedChar={state.selectedCharacter} />
+      <Character selectedChar={selectedCharacter} />
       <button onClick={sideHandler.bind(this, "light")}>Light Side</button>
       <button onClick={sideHandler.bind(this, "dark")}>Dark Side</button>
-      {state.side === "dark" && (
+      {chosenSide === "dark" && (
         <button onClick={destructionHandler}>DESTROY!</button>
       )}
     </React.Fragment>
   );
 
-  if (state.destroyed) {
+  if (destroyed) {
     content = <h1>Total destruction!</h1>;
   }
   return content;
